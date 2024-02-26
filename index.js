909fb97765e85ea1c9c3362a8fe75ca639562c8e
Yodgorbekova <marjonayodgorbekova505@gmail.com>
2024-02-26 14:00:58 +0500

    fixed

---
+++ index.js
@@ -0,0 +1,26 @@
+                                        //1-vazifa 
+//let a = +prompt("Raqam kiriting")
+//for (let i = 1; i <= a; i+=2) {
+ //console.log(i); 
+//}
+                                     
+
+
+
+                                        //2-vazifa
+//let a = +prompt("1-raqamni kiriting")
+//let b = +prompt("2-raqamni kiriting")
+
+//for (let i = 2; i > a && i <= b; i+=2) {
+ //console.log(i);
+//} 
+
+
+
+                                        //3-vazifa
+//let b = +prompt("Sarlavha kiriting")
+//let a = +prompt("Raqam kiriting")
+//for (let i = 1; i <= a ; i+=1) {
+  //console.log("Hello");
+    
+//} 
